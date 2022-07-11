0b4c88c079d1ab3763ad769f1c5c78d43eb68a9f
code PONPON <codeponpon@gmail.com>
2022-07-11 20:36:17 +0700

    feat: adding login feature

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,13 +10,14 @@ import { StorageService } from '../services/storage.service';
 })
 export class LoginComponent implements OnInit {
   form: any = {
-    username: null,
+    user_name: null,
     password: null,
   };
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
   roles: string[] = [];
+  currentUser: any = {};
 
   constructor(
     private authService: AuthService,
@@ -26,19 +27,26 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     if (this.storageService.isLoggedIn()) {
       this.isLoggedIn = true;
-      this.roles = this.storageService.getUser().roles;
+      this.currentUser = this.storageService.getUser();
+      this.roles = this.currentUser.userRole;
     }
   }
 
   onSubmit(): void {
-    const { username, password } = this.form;
-    this.authService.login(username, password).subscribe({
+    const { user_name, password } = this.form;
+    this.authService.login(user_name, password).subscribe({
       next: (data) => {
-        this.storageService.saveUser(data);
+        this.currentUser = {
+          ...data,
+          username: user_name,
+          userRole: 'user',
+        };
+        this.getProfile(data.access_token);
+        this.storageService.saveUser(this.currentUser);
         this.isLoginFailed = false;
         this.isLoggedIn = true;
-        this.roles = this.storageService.getUser().roles;
-        this.reloadPage();
+        this.roles = this.storageService.getUser().userRole;
+        // this.reloadPage();
       },
       error: (err) => {
         this.errorMessage = err.error.message;
@@ -49,5 +57,20 @@ export class LoginComponent implements OnInit {
 
   reloadPage(): void {
     window.location.reload();
+  }
+
+  getProfile(access_token: string): void {
+    this.authService.profile(access_token).subscribe({
+      next: (res) => {
+        this.currentUser.profile = {
+          ...res.data,
+          projects: res.data.active_projects,
+        };
+      },
+      error: (err) => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      },
+    });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const AUTH_API = 'http://vpmpttapi1.gptthailand.com';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
@@ -12,21 +13,27 @@ const httpOptions = {
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(
-      AUTH_API + 'signin',
-      {
-        username,
-        password,
-      },
-      httpOptions
-    );
+  login(user_name: string, password: string): Observable<any> {
+    return this.http
+      .post(
+        AUTH_API + '/api/v1/login',
+        {
+          user_name,
+          password,
+        },
+        httpOptions
+      )
+      .pipe(tap((_) => console.log('user login')));
   }
-  register(username: string, email: string, password: string): Observable<any> {
+  register(
+    user_name: string,
+    email: string,
+    password: string
+  ): Observable<any> {
     return this.http.post(
       AUTH_API + 'signup',
       {
-        username,
+        user_name,
         email,
         password,
       },
@@ -34,6 +41,34 @@ export class AuthService {
     );
   }
   logout(): Observable<any> {
-    return this.http.post(AUTH_API + 'signout', {}, httpOptions);
+    return this.http.post(AUTH_API + '/api/v1/logout', {}, httpOptions);
+  }
+  profile(access_token: string): Observable<any> {
+    httpOptions.headers = httpOptions.headers.set(
+      'Authorization',
+      `Bearer ${access_token}`
+    );
+    console.log('httpOptions', httpOptions);
+
+    return this.http
+      .get(AUTH_API + '/api/v1/users/me?includes[]=activeProjects', httpOptions)
+      .pipe(
+        tap((_) => console.log('fetched user profile')),
+        catchError(this.handleError('get profile', []))
+      );
+  }
+
+  // Private Method
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
